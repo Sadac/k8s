29107a89300cf52db7cd4fd24ff6d1f185b1fa30
Rocco <40279018+Sadac@users.noreply.github.com>
2021-12-30 16:31:40 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')()
-
-app.get('/', (req, res) => {
-    res.json({message: 'app is ready to go!'})
-})
-
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`app listening on port ${port}`))
